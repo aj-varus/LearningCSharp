ab8ea9f81db08e07d0a8d33eb484e567b2c5e0bb
Arjun Sen <arjunsen1997@gmail.com>
2021-07-04 01:23:26 +0530

    Started MVC project

---
+++ Foodly/Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using Foodly.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foodly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie { Name = "Shrek" };
+
+            return View(movie);
+        }
+    }
+}
--- RansomNote/Program.cs
+++ RansomNote/Program.cs
@@ -1,38 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
 namespace RansomNote
 {
-    public class Solution
-    {
-        public static void checkMagazine(List<string> magazine, List<string> note)
-        {
-            var dict = new Dictionary<string, int>();
-
-            foreach (var word in magazine)
-            {
-                if(dict.ContainsKey(word)){
-                    dict[word]++;
-                    continue;
-                }
-                dict.Add(word, 1);
-            }
-
-            foreach (var word in note)
-            {
-                if(!dict.ContainsKey(word) || dict[word] == 0) {
-                    Console.WriteLine("No");
-                    return;
-                }
-                dict[word]--;
-            }
-
-            Console.WriteLine("Yes");
-
-        }
-    }
     internal class Program
     {
         private static void Main(string[] args)
