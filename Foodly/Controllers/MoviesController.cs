741e1b4e76b8338b8982d8d1733059f745cdebce
Arjun Sen <arjunsen1997@gmail.com>
2021-07-04 21:37:25 +0530

    Exercise 1 complete.

--- Foodly/Controllers/MoviesController.cs
+++ Foodly/Controllers/MoviesController.cs
@@ -1,15 +1,62 @@
-﻿using Foodly.Models;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Foodly.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Foodly.Controllers
 {
     public class MoviesController : Controller
     {
+        private  List<Movie> Movies { get; set; }
+
+        public MoviesController()
+        {
+            Movies = new List<Movie>
+            {
+                new Movie
+                {
+                    Name = "Titanic",
+                    Id = 1
+                },
+
+                new Movie
+                {
+                    Name = "Tormentor",
+                    Id = 2
+                }
+            };
+        }
         public IActionResult Random()
         {
             var movie = new Movie { Name = "Shrek" };
 
             return View(movie);
         }
+
+        [Route("Movies/released/{year:int?}/{month:int?}")]
+        public IActionResult ByReleaseDate(int year = 2010, int month = 05)
+        {
+            return Content($"{year}/{month}");
+        }
+
+        public IActionResult Index()
+        {
+            return View(Movies);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var movie = Movies.SingleOrDefault(movie => movie.Id == id);
+
+            if (movie is null) return Content("Error 404");
+
+            return View(movie);
+
+        }
+
+
     }
 }
--- Foodly/Startup.cs
+++ Foodly/Startup.cs
@@ -1,13 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Foodly
 {
@@ -48,6 +43,8 @@ namespace Foodly
 
             app.UseEndpoints(endpoints =>
             {
+                //endpoints.MapControllers();
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
